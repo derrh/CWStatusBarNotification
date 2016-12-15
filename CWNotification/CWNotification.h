24a00b088865aa836e66dc0a6abbf341696a4ad8
Layne Moseley <lmoseley@instructure.com>
2016-12-15 12:02:09 -0700

    Add the header to the umbrella header

--- CWNotification/CWNotification.h
+++ CWNotification/CWNotification.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char CWNotificationVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CWNotification/PublicHeader.h>
 
-
+#import "CWStatusBarNotification.h"
